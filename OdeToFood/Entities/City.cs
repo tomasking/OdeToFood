9ccd976cb4f49a76ea88a08ef663327c5adc5ae7
Tomas King <tom@kingme.co.uk>
2017-04-20 07:58:37 +0100

    started entity framework

---
+++ OdeToFood/Entities/City.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using OdeToFood.Model;
+
+namespace OdeToFood.Entities
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public List<PointOfInterest> PointOfInterest { get; set; } = new List<PointOfInterest>();
+    }
+}
---
+++ OdeToFood/Entities/CityInfoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+    }
+}
